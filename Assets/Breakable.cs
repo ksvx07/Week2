0a98f0406742445121b4e3813e19afadb13b1e4d
RUTSIMT <rutsimt@gmail.com>
2025-09-11 21:43:10 +0900

    commit

--- Assets/Breakable.cs
+++ Assets/Breakable.cs
@@ -7,7 +7,7 @@ public class Breakable : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(me);
+        Destroy(gameObject);
         Debug.Log("adsjfkldasf");
         
     }
