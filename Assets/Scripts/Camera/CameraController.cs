1d2baee635e3269c31a839c663300dbd66509203
hanni01 <he3080@gmail.com>
2025-09-12 10:35:49 +0900

    [refact] CameraController 단차에 따른 follow 처리

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,11 +1,15 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Camera cam;
+    [SerializeField] private Camera Cam;
     [SerializeField] private Transform Player;
     [SerializeField] private float SmoothTime = 0.2f;
     [SerializeField] private CameraClamp Clamp;
+    [SerializeField] private float MinRiseJump = 5f;
+    [SerializeField] private float MinFollowHoldTime = 2f;
 
     [Header("Zoom In Out")]
     [SerializeField] private float MaxZoomIn = 3f;
@@ -16,6 +20,11 @@ public class CameraController : MonoBehaviour
     private Vector3 _offset = new Vector3(0, 0, -10);
     private Vector3 _velocity = new Vector3(2, 2, 2);
     private float targetZoom;
+    private Rigidbody2D _rb;
+    private float _beforeY;
+    private bool _followY;
+    private bool _wasHopping;
+    private float _hoppingEnterTime;
 
     public static CameraController instance = null;
     public bool IsTriggerZoom { get; private set; }
@@ -32,9 +41,22 @@ public class CameraController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        _rb = Player.GetComponent<Rigidbody2D>();
+        _beforeY = Player.position.y;
     }
 
     private void LateUpdate()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Update()
     {
         var desiredPos = HandleFollow();
 
@@ -48,8 +70,55 @@ public class CameraController : MonoBehaviour
 
     private Vector3 HandleFollow()
     {
-        Vector3 targetPos = Player.position + _offset;
-        return Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, SmoothTime);
+        /*Vector3 targetPos = Player.position + _offset;
+        return Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, SmoothTime);*/
+        float targetX = Player.position.x + _offset.x;
+        float newX = Mathf.SmoothDamp(transform.position.x, targetX, ref _velocity.x, SmoothTime);
+
+        // isHopping¿∫ TrianlgeController ø°º≠∏∏ √º≈©
+        bool isGround = Player.GetComponent<PlayerController>().IsGrounded;
+        //bool isHopping = Player.GetComponent<TrianglePlayerController>().IsHopping;
+        bool isJumping = Player.GetComponent<PlayerController>().IsJumping;
+
+        /*if (isHopping && !_wasHopping)
+        {
+            _hoppingEnterTime = Time.time;
+        }
+        _wasHopping = isHopping;*/
+
+        float newY = transform.position.y;
+
+        if (isGround)
+        {
+            _beforeY = Player.position.y;
+        }
+
+        float changedY = Player.position.y - _beforeY;
+
+        /*if (isHopping)
+        {
+            if (Time.time - _hoppingEnterTime > 2f)
+                _followY = true;
+            else
+                _followY = false;
+        }*/
+
+        if (Mathf.Abs(changedY) >= MinRiseJump)
+        {
+            _followY = true;
+        }
+        else if (isJumping)
+        {
+            _followY = true;
+        }
+
+        if (_followY)
+        {
+            float targetY = Player.position.y + _offset.y;
+            newY = Mathf.SmoothDamp(transform.position.y, targetY, ref _velocity.y, SmoothTime);
+        }
+
+        return new Vector3(newX, newY, transform.position.z);
     }
 
     private void HandleCameraShaking()
@@ -59,11 +128,10 @@ public class CameraController : MonoBehaviour
 
     private void HandleZoomInOut()
     {
-        var playerVelocity = Player.GetComponent<Rigidbody2D>().linearVelocity;
+        var playerVelocity = _rb.linearVelocity;
 
-        //if (Player.GetComponent<PlayerController>().IsJumping || !Player.GetComponent<PlayerController>().IsGrounded) return;
         _velocity = playerVelocity;
-        // ÔøΩÔøΩÔøΩÔøΩ ÔøΩÔøΩÔøΩ ÔøΩ”µÔøΩ ~ ÔøΩÃªÔøΩÔøΩÔøΩ ÔøΩÔøΩ ÔøΩÔøΩ ÔøΩ∆øÔøΩ »øÔøΩÔøΩ
+       
         float speed = playerVelocity.magnitude;
         if (speed > SpeedThreshold)
         {
@@ -74,18 +142,18 @@ public class CameraController : MonoBehaviour
             targetZoom = MaxZoomIn;
         }
 
-        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * ZoomLerpSpeed);
+        Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, targetZoom, Time.deltaTime * ZoomLerpSpeed);
     }
 
     public void TriggerZoomOut(float targetZoom)
     {
         IsTriggerZoom = true;
-        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * ZoomLerpSpeed);
+        Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, targetZoom, Time.deltaTime * ZoomLerpSpeed);
     }
 
     public void TriggerZoomIn(float targetZoom)
     {
-        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * ZoomLerpSpeed);
+        Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, targetZoom, Time.deltaTime * ZoomLerpSpeed);
         IsTriggerZoom = false;
     }
 }
