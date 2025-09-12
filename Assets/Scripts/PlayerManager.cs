cde65ff74ded8f06de13a06c3881fcdb86188440
hanni01 <he3080@gmail.com>
2025-09-12 23:37:35 +0900

    [feat&refact] 수정

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,39 +1,80 @@
-using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> Players;
+    [SerializeField] private List<GameObject> players;
     [SerializeField] private CameraController camControlelr;
+    [SerializeField] private GameObject selectPlayerPanel;
+
     private List<StageSwitch> StageSwitch;
     private GameObject _currentPlayer;
+    private PlayerInput inputActions;
+    private bool _isHold = false;
+    private Vector3 _scale = new Vector3(1.2f, 1.2f, 1.2f);
+    private float _selectPanelSpeed = 4f;
 
     private void Awake()
     {
+        inputActions = new PlayerInput();
         _currentPlayer = GameObject.FindWithTag("Player");
         StageSwitch = Object.FindObjectsByType<StageSwitch>(FindObjectsSortMode.None).ToList();
     }
 
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Hold.started += OnHold;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Hold.started -= OnHold;
+        inputActions.Player.Disable();
+    }
+
+    private void OnHold(InputAction.CallbackContext ctx)
+    {
+        _isHold = true;
+    }
+
     private void Update()
     {
-        // 키 눌렀을 때 변환 (임시)
+        // 키 눌렀을 때 변환
+        if (_isHold)
+        {
+            Time.timeScale = 0.3f;
+
+            selectPlayerPanel.SetActive(true);
+
+            Vector3.Lerp(selectPlayerPanel.transform.localScale, _scale, _selectPanelSpeed * Time.deltaTime);
+
+            // 키보드, 게임 패드 입력 들어와서 네비게이션으로 플레이어 선택되게 하기
+        }
+
         if (Input.GetKeyDown(KeyCode.W))
         {
-            int currentIdx = Players.IndexOf(_currentPlayer);
+            RapidChangePlayer();
+        }
+    }
 
-            if(currentIdx + 1  < Players.Count)
-            {
-                var nextPlayer = Players[currentIdx + 1];
+    private void RapidChangePlayer()
+    {
+        int currentIdx = players.IndexOf(_currentPlayer);
 
-                SetActivePlayer(_currentPlayer, nextPlayer);
-            }
-            else
-            {
-                var nextPlayer = Players[0];
+        if (currentIdx + 1 < players.Count)
+        {
+            var nextPlayer = players[currentIdx + 1];
 
-                SetActivePlayer(_currentPlayer, nextPlayer);
-            }
+            SetActivePlayer(_currentPlayer, nextPlayer);
+        }
+        else
+        {
+            var nextPlayer = players[0];
+
+            SetActivePlayer(_currentPlayer, nextPlayer);
         }
     }
 
