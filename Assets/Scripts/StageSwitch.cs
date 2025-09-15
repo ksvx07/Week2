9ee2c3d188cc7703bbb667cd4b1a8bf46d46474c
hanni01 <he3080@gmail.com>
2025-09-15 10:05:00 +0900

    [feat] Switch Map 코드 수정 - 나가고 들어오는 방향 체크 - 이전 이동 가능

--- Assets/Scripts/StageSwitch.cs
+++ Assets/Scripts/StageSwitch.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 
+public enum SwitchDirection
+{
+    LeftToRight,
+    RightToLeft,
+    TopToBottom,
+    BottomToTop
+}
+
 public class StageSwitch : MonoBehaviour
 {
     [SerializeField] private CameraClamp Clamp;
-    [SerializeField] private int Id;
+    [SerializeField] private int BeforeId;
+    [SerializeField] private int AfterId;
+    [SerializeField] private SwitchDirection direction;
 
     private Transform Player => PlayerManager.Instance._currentPlayerPrefab.transform;
 
@@ -11,9 +21,34 @@ public class StageSwitch : MonoBehaviour
     {
         if (Player == null) return;
 
-        if(Player.position.x > transform.position.x)
+        if (BeforeId == 0 || AfterId == 0) return;
+
+        switch (direction)
         {
-            Clamp.SetMapBounds(Id);
+            case SwitchDirection.LeftToRight:
+                if (Player.position.x > transform.position.x)
+                    Clamp.SetMapBounds(AfterId);
+                else if(Player.position.x < transform.position.x)
+                    Clamp.SetMapBounds(BeforeId);
+                break;
+            case SwitchDirection.RightToLeft:
+                if (Player.position.x < transform.position.x)
+                    Clamp.SetMapBounds(AfterId);
+                else if (Player.position.x > transform.position.x)
+                    Clamp.SetMapBounds(BeforeId);
+                break;
+            case SwitchDirection.TopToBottom:
+                if (Player.position.y < transform.position.y)
+                    Clamp.SetMapBounds(AfterId);
+                else if (Player.position.y > transform.position.y)
+                    Clamp.SetMapBounds(BeforeId);
+                break;
+            case SwitchDirection.BottomToTop:
+                if (Player.position.y > transform.position.y)
+                    Clamp.SetMapBounds(AfterId);
+                else if (Player.position.y < transform.position.y)
+                    Clamp.SetMapBounds(BeforeId);
+                break;
         }
     }
 }
